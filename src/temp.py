1c38b31aab7a4061f4dcc89c84340e5731aad0fe
Diego <dpalaciosarriaga@gmail.com>
2020-03-11 16:45:36 -0600

    working with variables

---
+++ src/temp.py
@@ -0,0 +1,15 @@
+PI = 3.14
+one, two, three = 1, 2, 3
+decimal = 1.4
+StringVar = "Hello"
+
+
+def FunctionName():
+    #global one
+    newVar = "World"
+    concVar = StringVar + newVar
+    print(concVar)
+    return
+
+
+FunctionName()
