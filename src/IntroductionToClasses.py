517ed0898782c5241188b43e9d9b3bd6646e75ae
Diego Palacios Arriaga <diego.palacios@aerobit.com>
2020-11-17 12:31:55 -0600

    IntroToClasses

--- src/IntroductionToClasses.py
+++ src/IntroductionToClasses.py
@@ -1,7 +1,7 @@
 class Team:
-    def __init__(self):
-        self.TeamName = "NaN"
-        self.TeamOrigin = "NaN"
+    def __init__(self, Name="No name", Origin="No origin"):
+        self.TeamName = Name
+        self.TeamOrigin = Origin
 
     def DefineTeamName(self, Name):
         self.TeamName = Name
@@ -10,11 +10,39 @@ class Team:
         self.TeamOrigin = Origin
 
 
-Team1 = Team()
+class Player(Team):
+    def __init__(self, PlayerName, PlayerPoints, TeamName, TeamOrigin):
+        Team.__init__(self, TeamName, TeamOrigin)
+        self.PlayerName = PlayerName
+        self.PlayerPoints = PlayerPoints
 
-print(Team1.TeamName)
+    def ScoredPoint(self):
+        self.PlayerPoints += 1
 
-Team1.DefineTeamName("Tigers")
-Team1.DefineTeamOrigin("Chicago")
-print(Team1.TeamName)
-print(Team1.TeamOrigin)
+    def setName(self, Name):
+        self.PlayerName = Name
+
+    def __str__(self):
+        return (
+            self.PlayerName
+            + " Player has scored: "
+            + str(self.PlayerPoints)
+            + " Points"
+        )
+
+
+Player1 = Player("Sid", 0, "Sharks", "Chicago")
+
+print(Player1.PlayerName)
+print(Player1.PlayerPoints)
+
+print(Player1.TeamName)
+print(Player1.TeamOrigin)
+
+Player1.ScoredPoint()
+print(Player1.PlayerPoints)
+
+Player1.setName("Lee")
+print(Player1.PlayerName)
+
+print(Player1)
